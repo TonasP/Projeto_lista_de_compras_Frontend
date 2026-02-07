4932a66917f920766c9eb4e80c709d205593b277
Thomaz <priamothomaz@gmail.com>
2026-02-07 04:07:38 -0300

    a

--- lista/lista.js
+++ lista/lista.js
@@ -1,26 +1,47 @@
 const API = "http://127.0.0.1:3000"
 
-async function listarItems(){
-    try{const response = await  fetch(`${API}/lista`,{
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
-    })
-    const data = await response.json
-    data.array.forEach(c => criarCard(c));
+let limit = 0
+let btnDeletar = document.querySelector('.delete')
+let btnFinalizar = document.querySelector('.confirm')
+document.addEventListener("DOMContentLoaded", () => {
+    listarItems()
+    selectCard()
+    
+})
+
+
+
+
+async function listarItems() {
+    const token = localStorage.getItem('tokenListaCompras');
+    try {
+        const response = await fetch(`${API}/lista?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json','Authorization': `Bearer ${token}` },
+            
+            
+        })
+        const data = await response.json()
+        const itens = Array.isArray(data) ? data : data.array;
+        itens.forEach(c => criarCard(c));
     }
-    catch(erro){
+    catch (erro) {
         console.log("Não foi possivel listar os cards", erro)
     }
 }
-async function criarCard(c){
+async function criarCard(c) {
+    let comentario = c.comentario
+     if (!comentario){   
+        comentario = 'Nenhuma instrução adicional.'
+    }
     const listaCards = document.getElementById('lista')
-    listaCards.innerHTML(`
-        <div class="cards">
+    listaCards.innerHTML+=`
+        <div class="cards" id = "cards">
                     <div class="lateral" id="lateral">
                         <p class="decoracaoLateral"></p>
                     </div>
                     <div class="checkboxCard" id="checkboxCard">
-                        <input type="checkbox">
+                        <input type="checkbox" class="checkbox">
                     </div>
                     <div class="meio">
                         <div class="topoInfos">
@@ -33,11 +54,40 @@ async function criarCard(c){
                             </div>
                         </div>
                         <div class="descricao" id="descricao">
-                            <p>testes</p>
+                            <p>${comentario}</p>
                         </div>
                     </div>
-
                 </div>
         `
-    )
 }
+function selectCard() {
+    const listagem = document.getElementById('lista'); 
+
+    listagem.addEventListener("click", (f) => {
+        
+        const cardClicado = f.target.closest('.cards');
+
+        if (!cardClicado) return;
+
+        const checkboxDoCard = cardClicado.querySelector('.checkbox');
+
+        if (checkboxDoCard) {
+            checkboxDoCard.classList.toggle('checkboxChecked');
+        }
+        cardClicado.classList.toggle('cardsChecked');
+        acionarBtnDeleteEAdd()
+    });
+}
+function acionarBtnDeleteEAdd(){
+    let checkCardAtivo = document.getElementsByClassName('cardsChecked')
+    let checkCheckboxAtivo = document.getElementsByClassName("checkboxChecked")
+    if (checkCardAtivo.length >0 || checkCheckboxAtivo.length >0 ){
+        btnDeletar.classList.add('deleteMostrar')
+        btnFinalizar.classList.add('addFinalizar')
+    }
+    else{
+        btnDeletar.classList.remove('deleteMostrar')
+        btnFinalizar.classList.remove('addFinalizar')
+    }
+}
+const listagem = document.getElementById("lista")
